3070c007f65251d0cfa8fc6ca1b6795c044280c0
Fuad <fuad.fuad.1984@list.ru>
2024-01-18 17:14:41 +0400

    url

--- src/shared/api/cardApi.js
+++ src/shared/api/cardApi.js
@@ -2,25 +2,25 @@ import axios from "axios";
 
 export const cardApi = {
     getCards: async () => {
-        const cards = await axios.get('http://13.49.133.140:5000/api/cards')
+        const cards = await axios.get('https://api.hobbi-nitki.ru/api/cards')
 
         return cards.data
     },
 
     getCardById: async (id) => {
-        const card = await axios.get(`http://13.49.133.140:5000/api/card/${id}`)
+        const card = await axios.get(`https://api.hobbi-nitki.ru/api/card/${id}`)
 
         return card.data
     },
 
     getCardByFilter: async (str1, str2) => {
-        const card = await axios.get(`http://13.49.133.140:5000/api/card/filter?section=${str1}&subsection=${str2}`)
+        const card = await axios.get(`https://api.hobbi-nitki.ru/api/card/filter?section=${str1}&subsection=${str2}`)
 
         return card.data
     },
 
     createCard: async (data) => {
-        const res = await axios.post(`http://13.49.133.140:5000/api/admin-add-card`, data, {
+        const res = await axios.post(`https://api.hobbi-nitki.ru/api/admin-add-card`, data, {
             headers: {
                 'content-type': 'multipart/form-data',
             }
